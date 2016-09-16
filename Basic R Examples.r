eee6c0ea92edcd8fac513435c0fa68b98d11acb9
jayape <jpertell@gmail.com>
2016-09-16 16:45:57 -0500

    Standardize quotes

--- Basic R Examples.r
+++ Basic R Examples.r
@@ -57,21 +57,21 @@ class(a)
 
 # Looping and comparisons
 if (1 == 1) {
-  print("1 = 1")
+  print('1 = 1')
 }
 
 if (1 == 2) {
-  print("1 = 2")
+  print('1 = 2')
 } else {
-  print ("1 != 2")
+  print ('1 != 2')
 }
 
 if (1 == 2) {
-  print("1 = 2")
+  print('1 = 2')
 } else if (1 != 1){
-  print ("1 != 1")
+  print ('1 != 1')
 } else {
-  print ("1 must = 1")
+  print ('1 must = 1')
 }
   
 
